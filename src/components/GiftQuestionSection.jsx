c0a5ed166307831978ad97ff07c4a870d581fbef
raj1799 <105409949+raj1799@users.noreply.github.com>
2025-12-04 01:46:28 +0530

    question and answer ection

---
+++ src/components/GiftQuestionSection.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "../styles/giftQuestion.css";
+
+export default function GiftQuestionSection() {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const qnaList = [
+    {
+      q: "What do you miss the most? 💭",
+      a: "A voice… a smile… and someone who still feels like home 💞",
+    },
+    {
+      q: "Who do you look for first on a bad day? 🌧️",
+      a: "Don’t lie… your mind just whispered my name 😉❤️",
+    },
+    {
+      q: "What do you hide behind your silence? 🤫",
+      a: "A feeling you’re scared to admit… but I already feel it too 💗",
+    },
+    {
+      q: "If love had one last message for you… 💌",
+      a: "You can push me away, but I will still choose you… every single time 💘",
+    },
+    {
+        q:"If climate was good that day… what made your heart think of me first? 😏🌤️",
+        a:"Come on… it wasn’t the weather.It was your heart looking for a reason to hear my voice. 💕"
+    },
+    {
+        q:"Ready to see a few memories?",
+        a:"These are a piece of my heart — take care of them."
+    }
+  ];
+
+  return (
+    <div className="qna-section">
+      <h2 className="qna-title">Before We Go Further… 💖</h2>
+
+      <div className="qna-list">
+        {qnaList.map((item, i) => (
+          <div
+            key={i}
+            className={`qna-card ${openIndex === i ? "open" : ""}`}
+            onClick={() => setOpenIndex(i)}
+          >
+            {/* CLOSED STATE */}
+            {openIndex !== i && (
+              <div className="qna-closed">
+                <div className="qna-ribbon"></div>
+                <p className="qna-hint">🎁 Tap to Open</p>
+                <p className="qna-question-preview">{item.q}</p>
+              </div>
+            )}
+
+            {/* OPEN STATE */}
+            {openIndex === i && (
+              <div className="qna-open-content">
+                <h3>{item.q}</h3>
+                <p>{item.a}</p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,13 +3,14 @@ import Countdown from "../components/Countdown";
 import GiftBoxSection from "../components/GiftBoxSection";
 import PolaroidsSection from "../components/PolaroidSection";
 import SurpriseCarousel from "../components/SurpriseCarousel";
-
+import GiftQuestionSection from "../components/GiftQuestionSection";
 export default function LandingPage() {
   return (
     <div className="landing-container">
       <Countdown />
-      <PolaroidsSection/>
-      <SurpriseCarousel/> 
+      <GiftQuestionSection /> 
+       <SurpriseCarousel/>
+      <PolaroidsSection/> 
       <GiftBoxSection/>
        {/* <BirthdayCake/> */}
     </div>
